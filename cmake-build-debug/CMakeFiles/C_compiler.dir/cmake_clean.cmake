b4061074bdfab2dde624ec891e1074d9c9b3d86b
yokel <1426742045@qq.com>
2021-11-03 23:52:15 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/C_compiler.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_compiler.dir/main.cpp.obj"
+  "C_compiler.exe"
+  "C_compiler.exe.manifest"
+  "C_compiler.lib"
+  "C_compiler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C_compiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
